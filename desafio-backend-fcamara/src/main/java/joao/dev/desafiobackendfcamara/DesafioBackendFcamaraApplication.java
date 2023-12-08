0eeeb79cb410844b50133b382b486856595fbfb8
João Pedro Faria <joaopedropepe56@gmail.com>
2023-12-07 21:48:09 -0300

    First Commit

---
+++ desafio-backend-fcamara/src/main/java/joao/dev/desafiobackendfcamara/DesafioBackendFcamaraApplication.java
@@ -0,0 +1,13 @@
+package joao.dev.desafiobackendfcamara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendFcamaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendFcamaraApplication.class, args);
+	}
+
+}
---
+++ desafio-backend-fcamara/src/test/java/joao/dev/desafiobackendfcamara/DesafioBackendFcamaraApplicationTests.java
@@ -0,0 +1,13 @@
+package joao.dev.desafiobackendfcamara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendFcamaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
